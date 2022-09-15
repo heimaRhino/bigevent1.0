b3c25b979941acdf9931b850a2b1deb788d0b5de
itzai <1835442107@qq.com>
2022-09-15 13:36:22 +0800

    完成卡主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,18 @@
 $.ajaxPrefilter(function (xx) {
-  xx.url = "http://127.0.0.1:3070" + xx.url;
+  xx.url = "http://127.0.0.1:3007" + xx.url;
+  if (xx.url.indexOf("/my/") !== -1) {
+    xx.headers = {
+      Authorization: localStorage.getItem("token") || " ",
+    };
+  }
+  xx.complete = function (res) {
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败"
+    ) {
+      localStorage.removeItem("token");
+
+      location.href = "/login.html";
+    }
+  };
 });
--- assets/js/login.js
+++ assets/js/login.js
@@ -28,7 +28,7 @@ $(function () {
     e.preventDefault();
     $.post(
       // "http://ajax.frontend.itheima.net/api/reguser",
-      "api/reguser",
+      "/api/reguser",
       {
         username: $("#form-reg [name=username]").val(),
         password: $("#form-reg [name=password]").val(),
@@ -41,21 +41,44 @@ $(function () {
       }
     );
   });
+  // 监听登录表单的提交事件
   $("#form-login").submit(function (e) {
+    // 阻止默认提交行为
     e.preventDefault();
     $.ajax({
-      url: " /api/login",
+      url: "/api/login",
       method: "POST",
-      data: $(this).serialize(), //快速获取表单中的数据
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
       success: function (res) {
         if (res.status !== 0) {
-          return layer.msg("登陆失败");
+          return layer.msg("登录失败！");
         }
-
-        layer.msg("登陆成功");
+        layer.msg("登录成功！");
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem("token", res.token);
+        // 跳转到后台主页
         location.href = "/index.html";
       },
     });
   });
+  // $("#form-login").submit(function (e) {
+  //   e.preventDefault();
+  //   console.log("1");
+  //   $.ajax({
+  //     url: " /api/login",
+  //     method: "POST",
+
+  //     data: $(this).serialize(), //快速获取表单中的数据
+  //     success: function (res) {
+  //       if (res.status !== 0) {
+  //         return layer.msg("登陆失败");
+  //       }
+
+  //       layer.msg("登陆成功");
+  //       localStorage.setItem("token", res.token);
+  //       location.href = "/index.html";
+  //     },
+  //   });
+  // });
 });
